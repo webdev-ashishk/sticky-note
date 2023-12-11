289938503753c36e2d7aa1d0b9a7edb037caf14b
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-11 11:00:13 +0530

    fetched data not showing

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 "use client";
 import StickyNote from "@/components/stickyNote";
+import { MyContext } from "@/context/myContext";
 import { useState } from "react";
 import { CiCirclePlus } from "react-icons/ci";
-     
+
 export default function Home() {
   const [notes, setNotes] = useState([]);
   function addNote() {
@@ -20,35 +21,37 @@ export default function Home() {
   };
   console.log(notes);
   return (
-    <main>
-      {/* title  */}
-      <div>
-        <h1 className="text-5xl font-extrabold bg-gradient-to-br from-blue-800 to-red-200 bg-clip-text text-transparent">
-          QUICK-NOTE
-        </h1>
-      </div>
-      {/* button  */}
-      <div className="text-5xl font-bold text-blue-400 flex justify-center items-center">
-        <button onClick={addNote}>
-          <CiCirclePlus />
-        </button>
-      </div>
-      {/* textarea  */}
-      <div className="flex flex-wrap justify-center items-center">
-        {notes &&
-          notes.map((note) => {
-            return (
-              <div key="note.id">
-                <StickyNote
-                  id={note.id}
-                  text={note.text}
-                  onUpdate={updateNote}
-                  onDelete={() => deleteNote(note.id)}
-                />
-              </div>
-            );
-          })}
-      </div>
-    </main>
+    <MyContext.Provider value={{ notes, setNotes }}>
+      <main>
+        {/* title  */}
+        <div>
+          <h1 className="text-5xl font-extrabold bg-gradient-to-br from-blue-800 to-red-200 bg-clip-text text-transparent">
+            QUICK-NOTE
+          </h1>
+        </div>
+        {/* button  */}
+        <div className="text-5xl font-bold text-blue-400 flex justify-center items-center">
+          <button onClick={addNote}>
+            <CiCirclePlus />
+          </button>
+        </div>
+        {/* textarea  */}
+        <div className="flex flex-wrap justify-center items-center">
+          {notes &&
+            notes.map((note) => {
+              return (
+                <div key="note.id">
+                  <StickyNote
+                    id={note.id}
+                    text={note.text}
+                    onUpdate={updateNote}
+                    onDelete={() => deleteNote(note.id)}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      </main>
+    </MyContext.Provider>
   );
 }
